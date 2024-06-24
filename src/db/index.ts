859c938efe2098595ab1f023b3ccad49d07c1a3b
Abdeslam Ezzaghi <60900845+AbdeslamEzzaghi@users.noreply.github.com>
2024-06-25 00:01:00 +0100

    fixed drizzle error

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,9 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
+import * as schema from "./schema"
 import postgres from 'postgres';
 
 
 const queryClient = postgres(process.env.DATABASE_URL!);
-const db = drizzle(queryClient);
+const db = drizzle(queryClient,{schema});
 
 export {db};
