64cb793102fb4f1943ccca68a681af9be8fa8764
Abdeslam Ezzaghi <60900845+AbdeslamEzzaghi@users.noreply.github.com>
2024-06-24 23:47:52 +0100

    finished the db setup

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,7 +2,7 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 
 
-const queryClient = postgres("postgres://postgres:adminadmin@0.0.0.0:5432/db");
+const queryClient = postgres(process.env.DATABASE_URL!);
 const db = drizzle(queryClient);
 
 export {db};
